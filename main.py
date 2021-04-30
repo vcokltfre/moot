769aace1c9bddecbb5c708eadd0690ed5214e965
vcokltfre <vcokltfre@gmail.com>
2021-04-30 21:21:45 +0100

    feat: load dotenv on start if not in docker

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from os import getenv
+
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
 
 from src.routing import frontend_router
 
+
+if not getenv("IN_DOCKER"):
+    load_dotenv()
 
 app = FastAPI(docs_url=None, redoc_url=None, apoenapi_url=None)
 app.mount("/static", StaticFiles(directory="static"), "static")
