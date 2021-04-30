26e6bdaf99c25e5766b1bf58bc99821a973d6ebe
vcokltfre <vcokltfre@gmail.com>
2021-04-30 21:22:12 +0100

    feat: add basic site content

--- src/routing/frontend.py
+++ src/routing/frontend.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -7,6 +9,9 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/")
+@router.get("/login")
 async def get_index(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("login.html", {
+        "request": request,
+        "discord_oauth": getenv("OAUTH_URL"),
+    })
