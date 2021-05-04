a904acc7617d9b02cd14e4aa883c15607084fe39
vcokltfre <vcokltfre@gmail.com>
2021-05-04 21:53:43 +0100

    feat: add oauth callback logic

--- src/routing/api/oauth.py
+++ src/routing/api/oauth.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
 
 from src.utils.oauth import get_user_details
 
@@ -6,9 +7,17 @@ from src.utils.oauth import get_user_details
 router = APIRouter()
 
 @router.get("/callback")
-async def auth_callback(request: Request) -> Response:
+async def auth_callback(request: Request) -> RedirectResponse:
     """Authenticate the user with Discord OAuth."""
 
     user = await get_user_details(request.state.session, request.query_params["code"])
 
-    print(user)
+    userid = int(user["id"])
+    username = user["username"] + user["discriminator"]
+    avatar = user.get("avatar", None)
+
+    session = await request.state.db.user_login(userid, username, avatar)
+
+    response = RedirectResponse("/")
+    response.set_cookie("moot_session_token", session.token)
+    return response
