50b5b62da1bb3a1cc6108ccd7a10a8422f893c07
vcokltfre <vcokltfre@gmail.com>
2021-05-04 22:13:51 +0100

    feat: add request_auth method to authstate

--- src/utils/datamodels.py
+++ src/utils/datamodels.py
@@ -1,6 +1,9 @@
+from os import getenv
 from typing import Optional
 from datetime import datetime
 from dataclasses import dataclass, field
+
+from fastapi.responses import RedirectResponse
 
 
 @dataclass
@@ -34,3 +37,6 @@ class Session:
 class AuthState:
     user: User = field(default=None)
     session: Session = field(default=None)
+
+    def request_auth(self) -> RedirectResponse:
+        return RedirectResponse(getenv("OAUTH_URL"))
